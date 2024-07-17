9be56675e2eac46edd13213015fea5af969aa919
MarufHassanPietech <maruf@pietechsolution.com>
2024-07-17 19:56:25 +0530

    Added colors

---
+++ lib/constants/colors.const.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  const AppColors._();
+
+  static const Color primaryBlue = Color(0xFF0C54BE);
+  static const Color secondaryDimBlue = Color(0xFF303F60);
+  static const Color lightGrey = Color(0xFFF5F9FD);
+  static const Color darkGrey = Color(0xFFCED3DC);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
