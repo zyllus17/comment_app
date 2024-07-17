4f84969b61da65fc76335d026717d0566a222183
MarufHassanPietech <maruf@pietechsolution.com>
2024-07-18 02:06:09 +0530

    Code improvement

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,20 @@ import 'package:comment_app/firebase_options.dart';
 import 'package:comment_app/page/home/home.ui.dart';
 import 'package:comment_app/page/login/login.ui.dart';
 import 'package:comment_app/page/signup/signup.ui.dart';
+import 'package:comment_app/services/auth.service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    debugPrint('Firebase initialization error: $e');
+  }
   runApp(const MyApp());
 }
 
@@ -19,20 +24,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-        // MultiProvider(
-        //   providers: [
-        //     // Add your providers here
-        //   ],
-        // child:
-        MaterialApp(
-      title: 'Comments App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthService()),
+      ],
+      child: MaterialApp(
+        title: 'Comments App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Consumer<AuthService>(
+                builder: (context, authService, _) {
+                  return authService.user == null
+                      ? LoginScreen()
+                      : HomeScreen();
+                },
+              ),
+          '/signup': (context) => SignupScreen(),
+          '/login': (context) => LoginScreen(),
+          '/home': (context) => HomeScreen(),
+        },
       ),
-      home: HomeScreen(),
-      // LoginScreen(),
-      // ),
     );
   }
 }
