7dcc2a878db6fb787359c13a870ea7e92ac889f1
MarufHassanPietech <maruf@pietechsolution.com>
2024-07-17 19:24:39 +0530

    Initialized Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# comment_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/comment_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.comment_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
