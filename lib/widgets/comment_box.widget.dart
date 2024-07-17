72a74750adc0878e78d7a85136ec86ac6bd04a6d
MarufHassanPietech <maruf@pietechsolution.com>
2024-07-17 22:54:32 +0530

    Created CommentBox widget

---
+++ lib/widgets/comment_box.widget.dart
@@ -0,0 +1,106 @@
+import 'package:comment_app/constants/colors.const.dart';
+import 'package:flutter/material.dart';
+
+class CommentBox extends StatelessWidget {
+  final String initial;
+  final String name;
+  final String email;
+  final String comment;
+  const CommentBox({
+    super.key,
+    required this.initial,
+    required this.name,
+    required this.email,
+    required this.comment,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CircleAvatar(
+            radius: 25,
+            backgroundColor: AppColors.primaryBlue,
+            child: Text(
+              initial,
+              style: theme.textTheme.headlineSmall?.copyWith(
+                color: AppColors.white,
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: 'Name: ',
+                      style: theme.textTheme.headlineSmall?.copyWith(
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 16,
+                        color: AppColors.darkGrey,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: name,
+                          style: theme.textTheme.headlineSmall?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Email: ',
+                      style: theme.textTheme.headlineSmall?.copyWith(
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 16,
+                        color: AppColors.darkGrey,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: email,
+                          style: theme.textTheme.headlineSmall?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    comment,
+                    style: theme.textTheme.headlineSmall?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                    maxLines: 4,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
