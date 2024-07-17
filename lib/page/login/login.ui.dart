b6065500845fcbd8bc1aa0178b24bbf69b9853eb
MarufHassanPietech <maruf@pietechsolution.com>
2024-07-18 02:06:50 +0530

    Added logic for login button

--- lib/page/login/login.ui.dart
+++ lib/page/login/login.ui.dart
@@ -1,6 +1,9 @@
 import 'package:comment_app/constants/colors.const.dart';
+import 'package:comment_app/page/home/home.ui.dart';
 import 'package:comment_app/page/signup/signup.ui.dart';
+import 'package:comment_app/services/auth.service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
@@ -47,6 +50,7 @@ class LoginScreen extends StatelessWidget {
             const SizedBox(height: 20),
             TextFormField(
               controller: _passwordController,
+              obscureText: true,
               decoration: InputDecoration(
                 labelText: 'Password',
                 fillColor: AppColors.white,
@@ -63,11 +67,32 @@ class LoginScreen extends StatelessWidget {
             SizedBox(
               width: 250,
               child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignupScreen()),
-                  );
+                onPressed: () async {
+                  final String email = _emailController.text.trim();
+                  final String password = _passwordController.text.trim();
+
+                  if (email.isNotEmpty && password.isNotEmpty) {
+                    try {
+                      await Provider.of<AuthService>(context, listen: false)
+                          .login(email, password);
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                      );
+                      debugPrint('Logged in');
+                    } catch (e) {
+                      // Print error to debug console
+                      print('Login error: $e');
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Failed to sign in')),
+                      );
+                    }
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                          content: Text('Please enter email and password')),
+                    );
+                  }
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: AppColors.primaryBlue,
