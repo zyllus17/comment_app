37c73152c683b4b355894536e7b514421c21749f
MarufHassanPietech <maruf@pietechsolution.com>
2024-07-18 00:20:11 +0530

    Added logic to fetch Comments

--- lib/page/home/home.ui.dart
+++ lib/page/home/home.ui.dart
@@ -1,45 +1,74 @@
+import 'package:comment_app/models/comment.model.dart';
+import 'package:comment_app/services/comment.service.dart';
 import 'package:comment_app/widgets/comment_box.widget.dart';
 import 'package:flutter/material.dart';
 
 import 'package:comment_app/constants/colors.const.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Comment>> futureComments;
+
+  @override
+  void initState() {
+    super.initState();
+    futureComments = CommentService().fetchComments();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
-        backgroundColor: AppColors.lightGrey,
-        appBar: AppBar(
-          centerTitle: false,
-          backgroundColor: AppColors.primaryBlue,
-          title: Text(
-            'Comments',
-            style: theme.textTheme.headlineSmall?.copyWith(
-              fontWeight: FontWeight.bold,
-              color: AppColors.white,
-              fontSize: 20,
-            ),
+      backgroundColor: AppColors.lightGrey,
+      appBar: AppBar(
+        centerTitle: false,
+        backgroundColor: AppColors.primaryBlue,
+        title: Text(
+          'Comments',
+          style: theme.textTheme.headlineSmall?.copyWith(
+            fontWeight: FontWeight.bold,
+            color: AppColors.white,
+            fontSize: 20,
           ),
         ),
-        body: CustomScrollView(
-          slivers: [
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                childCount: 10,
-                (context, index) {
-                  return const CommentBox(
-                    initial: 'A',
-                    name: 'Maruf',
-                    email: 'maruf@pingolearn.com',
-                    comment:
-                        'Hello there what a wonderful day. Lorum impusdaskj',
-                  );
-                },
-              ),
-            ),
-          ],
-        ));
+      ),
+      body: FutureBuilder<List<Comment>>(
+        future: futureComments,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data?.isEmpty == true) {
+            return const Center(child: Text('No comments found'));
+          } else {
+            return CustomScrollView(
+              slivers: [
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      final comment = snapshot.data![index];
+                      return CommentBox(
+                        initial: comment.name[0].toUpperCase(),
+                        name: comment.name,
+                        email: comment.email,
+                        comment: comment.body,
+                      );
+                    },
+                    childCount: snapshot.data?.length,
+                  ),
+                ),
+              ],
+            );
+          }
+        },
+      ),
+    );
   }
 }
