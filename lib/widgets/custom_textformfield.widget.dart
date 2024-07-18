5e9478491c684cc731ae715aacaedfb7148705cf
MarufHassanPietech <maruf@pietechsolution.com>
2024-07-18 16:52:59 +0530

    Created CustomTextFormField

---
+++ lib/widgets/custom_textformfield.widget.dart
@@ -0,0 +1,42 @@
+import 'package:comment_app/constants/colors.const.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final String? Function(String?)? validator;
+  final bool? obscureText;
+
+  const CustomTextFormField({
+    Key? key,
+    required this.controller,
+    required this.label,
+    required this.validator,
+    this.obscureText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText ?? false,
+      decoration: InputDecoration(
+        labelText: label,
+        fillColor: AppColors.white,
+        filled: true,
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide.none,
+        ),
+        errorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 2.0),
+        ),
+        focusedErrorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 2.0),
+        ),
+      ),
+      validator: validator,
+    );
+  }
+}
