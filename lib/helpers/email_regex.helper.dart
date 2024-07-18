1b0e09ea16bfc2b5a133290357cc11a6fbd5d4d2
MarufHassanPietech <maruf@pietechsolution.com>
2024-07-18 16:52:48 +0530

    Created helper function

---
+++ lib/helpers/email_regex.helper.dart
@@ -0,0 +1,5 @@
+bool isValidEmail(String email) {
+  // Regular expression for a basic email validation
+  final RegExp emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+  return emailRegex.hasMatch(email);
+}
